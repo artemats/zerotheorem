4a26fbb03f4b6dba4c089cf65af415efab0244a7
Артем Мацерук <artem.matseruk@gmail.com>
2020-06-18 13:08:29 +0300

    check CORS access

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -23,7 +23,10 @@ class App extends Component {
     render() {
         return(
             <div>
-
+                <h1 className="title-1">Quant lab</h1>
+                <h2 className="title-2">Wtf...is zt?</h2>
+                <h3 className="title-3">A Gated Recurrent Unit Approach to Bitcoin Price P-1</h3>
+                <p>Traditional neural networks have shortcomings in effectively using prior information for future predictions of Bitcoin price. Therefore, Dutta et al. (2020) investigated a framework</p>
             </div>
         )
     }
