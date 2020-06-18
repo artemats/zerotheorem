25f140753fca3ceef8fa3326d76d12954be74dee
Артем Мацерук <artem.matseruk@gmail.com>
2020-06-18 12:12:23 +0300

    first commit

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -23,10 +23,7 @@ class App extends Component {
     render() {
         return(
             <div>
-                <h1 className="regular">Hello Zero</h1>
-                <h1 className="medium">Hello Zero</h1>
-                <h1 className="bold">Hello Zero</h1>
-                <p className="light">Hello Zero</p>
+
             </div>
         )
     }
