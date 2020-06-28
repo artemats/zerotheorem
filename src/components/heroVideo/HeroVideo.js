244e82a5602eecb9685d0796383830580a019769
artemats <artem.matseruk@gmail.com>
2020-06-28 12:46:01 +0300

    optimize hero video load

--- src/components/heroVideo/HeroVideo.js
+++ src/components/heroVideo/HeroVideo.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import video from '../../assets/video/hero.mp4';
 
 const HeroVideo = () => {
 
+    const [onLoaded, setOnLoaded] = useState(false);
+
+    useEffect(() => {
+
+        setOnLoaded(true);
+
+    });
+
+    if(!onLoaded) {
+
+        return <div>Loading...</div>
+
+    }
+
     return(
         <div className="hero-video">
-            <video className="hero-video-item" width="100%" height="100%" loop muted playsInline autoPlay>
-                <source type="video/mp4" src={video} />
+            <video
+                className="hero-video-item"
+                width="100%"
+                height="100%"
+                preload={'auto'}
+                loop
+                muted
+                playsInline
+                autoPlay>
+                <source src={video} type="video/mp4" />
             </video>
         </div>
     )
