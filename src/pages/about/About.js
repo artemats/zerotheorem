e205213a55b93b9843212d14e7dfaa3a1d48b5b0
artemats <artem.matseruk@gmail.com>
2020-07-21 14:45:25 +0300

    resources page, funding model

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './about.scss';
-import icon1 from '../../assets/images/about/1.png';
-import icon2 from '../../assets/images/about/2.png';
-import icon3 from '../../assets/images/about/3.png';
+import Mission from "./mission/Mission";
 
 const About = () => {
 
@@ -42,67 +40,7 @@ const About = () => {
                     </div>
                 </div>
             </div>
-            <div className="mission">
-                <div className="abstract-circle __1" />
-                <div className="abstract-circle __2" />
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12">
-                            <div className="hero-title">
-                                <p className="subtitle">0100100001000101010011000100110001001111</p>
-                                <h3 className="title-1 title-border">Our mission</h3>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-lg-4">
-                            <div className="mission-box" data-num="01">
-                                <div className="mission-box-icon">
-                                    <img src={icon1} alt="Zero BIAS" />
-                                </div>
-                                <div className="mission-box-title">
-                                    <h3 className="title-3">Zero BIAS</h3>
-                                </div>
-                                <div className="mission-box-description">
-                                    <ul>
-                                        <li>We review of all academic bitcoin pricing models.</li>
-                                        <li>A replication of data features, model selection and calibration with independent back testing analysis.</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-4">
-                            <div className="mission-box" data-num="02">
-                                <div className="mission-box-icon">
-                                    <img src={icon2} alt="Zero Cost" />
-                                </div>
-                                <div className="mission-box-title">
-                                    <h3 className="title-3">Zero Cost</h3>
-                                </div>
-                                <div className="mission-box-description">
-                                    <ul>
-                                        <li>We provide a free bitcoin pricing benchmark.</li>
-                                    </ul>
-                                    <p>In Quantlab both baseline and meta-model forecasts, model statistics, market metrics, and a range of alternative insights are presented for the community.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-4">
-                            <div className="mission-box" data-num="03">
-                                <div className="mission-box-icon">
-                                    <img src={icon3} alt="Zero Theorem" />
-                                </div>
-                                <div className="mission-box-title">
-                                    <h3 className="title-3">Zero Theorem</h3>
-                                </div>
-                                <div className="mission-box-description">
-                                    <p>Via our baseline forecast models or advanced aggregation stack in Quant Lab</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Mission />
             <div className="infrastructure">
                 <div className="abstract-circle __1" />
                 <div className="abstract-circle __4" />
--- src/pages/resources/Resources.js
+++ src/pages/resources/Resources.js
@@ -1,11 +1,99 @@
 import React from 'react';
+import Mission from "../about/mission/Mission";
+import './resources.scss';
+import icon1 from '../../assets/images/icons/subscribers.png';
+import icon2 from '../../assets/images/icons/lab.png';
+import icon3 from '../../assets/images/icons/research.png';
 
 const Resources = () => {
 
     return(
-        <div>
-            <h1>Resources page</h1>
-        </div>
+        <section className="section hero inner-page">
+            <Mission />
+            <div className="funding-model">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="hero-title">
+                                <p className="subtitle">Useful</p>
+                                <h3 className="title-1 title-border">Our funding model</h3>
+                                <p className="hero-title-description">“Our subscribers finance the research and development of future economic theory…”</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="funding-model-box">
+                                <div className="model">
+                                    <div className="model-area __1" />
+                                    <div className="model-area __2" />
+                                    <div className="model-area __3" />
+                                    <div className="model-header">
+                                        <div className="model-header-item __1">
+                                            <p className="regular">Global Community</p>
+                                        </div>
+                                        <div className="model-header-item __2">
+                                            <p className="regular">Zero Theorem</p>
+                                        </div>
+                                    </div>
+                                    <div className="model-row">
+                                        <div className="model-row-item">
+                                            <div className="model-row-box">
+                                                <p>$$$</p>
+                                                <div className="model-arrow right" />
+                                            </div>
+                                        </div>
+                                        <div className="model-row-item">
+                                            <div className="model-row-box">
+                                                <p>$</p>
+                                                <div className="model-arrow right" />
+                                            </div>
+                                            <div className="model-row-box">
+                                                <p>Infrastructure</p>
+                                            </div>
+                                            <div className="model-row-box">
+                                                <p>Data & Models</p>
+                                                <div className="model-arrow right" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="model-row">
+                                        <div className="model-row-detail">
+                                            <img src={icon1} alt="Subscribers" />
+                                            <p className="regular">Subscribers</p>
+                                        </div>
+                                        <div className="model-row-box">
+                                            <p>Advanced Forecasts</p>
+                                            <div className="model-arrow model-arrow-left left" />
+                                        </div>
+                                        <div className="model-row-detail">
+                                            <img src={icon2} alt="Quant Lab" />
+                                            <p className="regular">Quant Lab</p>
+                                        </div>
+                                        <div className="model-row-box">
+                                            <p>Satoshi Stack Enhancements</p>
+                                            <div className="model-arrow model-arrow-left left" />
+                                        </div>
+                                        <div className="model-row-detail">
+                                            <img src={icon3} alt="Research" />
+                                            <p className="regular">Research</p>
+                                        </div>
+                                    </div>
+                                    <div className="model-row">
+                                        <div className="model-row-item">
+                                            <div className="model-row-box">
+                                                <p>Improve Baseline Valuation Model + Economic Research Pipeline</p>
+                                                <div className="model-arrow model-arrow-left left" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 
 };
