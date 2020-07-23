261bc083cc89d3e75ae2ac67e9b29b17db83f497
artemats <artem.matseruk@gmail.com>
2020-07-23 12:54:43 +0300

    resources page

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './about.scss';
 import Mission from "./mission/Mission";
+import Manifesto from "./manifesto/Manifesto";
 
 const About = () => {
 
@@ -205,101 +206,7 @@ const About = () => {
                     </div>
                 </div>
             </div>
-            <div className="manifesto">
-                <div className="abstract __12" />
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12">
-                            <div className="hero-title">
-                                <p className="subtitle">We will make clear</p>
-                                <h3 className="title-1 title-border">OUR manifesto</h3>
-                            </div>
-                            <div className="hero-title-description __quote">
-                                <p className="quote-title">
-                                    “…Inspired by the Modelers’ Hippocratic Oath”
-                                    <span className="quote-author">By Dr. Emanuel Derman and Dr. Paul Wilmott</span>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="manifesto-list">
-                        <div className="row">
-                            <div className="col-lg-8 offset-lg-3">
-                                <div className="manifesto-list-item">
-                                    <div className="manifesto-box" data-num="01">
-                                        <div className="manifesto-box-title">
-                                            <h3 className="title-3">Stochastic properties</h3>
-                                        </div>
-                                        <div className="manifesto-box-description">
-                                            <p>Even though we use a combination of statistical, econometric and machine learning methods to estimate value, we will never overlook the stochastic properties of reality for the pristine beauty of mathematical formulation</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-8 offset-lg-3">
-                                <div className="manifesto-list-item">
-                                    <div className="manifesto-box" data-num="02">
-                                        <div className="manifesto-box-title">
-                                            <h3 className="title-3">We will make clear</h3>
-                                        </div>
-                                        <div className="manifesto-box-description">
-                                            <p>We will make clear the assumptions, oversights and accuracy of our models to those who intent to use them and never claim that th ey have the full explanatory insights into real world market dynamics.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-8 offset-lg-3">
-                                <div className="manifesto-list-item">
-                                    <div className="manifesto-box" data-num="03">
-                                        <div className="manifesto-box-title">
-                                            <h3 className="title-3">We understand</h3>
-                                        </div>
-                                        <div className="manifesto-box-description">
-                                            <p>We understand that our models can provoke past, current and future economic theory and accept that our work may result in consequences on society that can be far beyond our own comprehension.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-8 offset-lg-3">
-                                <div className="manifesto-list-item">
-                                    <div className="manifesto-box" data-num="04">
-                                        <div className="manifesto-box-title">
-                                            <h3 className="title-3">We commit</h3>
-                                        </div>
-                                        <div className="manifesto-box-description">
-                                            <p>We commit to continuously improving and evolving our models and will never claim that they are complete.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-8 offset-lg-3">
-                                <div className="manifesto-list-item">
-                                    <div className="manifesto-box" data-num="05">
-                                        <div className="manifesto-box-title">
-                                            <h3 className="title-3">We dedicate</h3>
-                                        </div>
-                                        <div className="manifesto-box-description">
-                                            <p>We dedicate ourselves to expanding the current body of knowledge by providing our own academic research and releasing this information to the public at no cost.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-8 offset-lg-3">
-                                <div className="manifesto-list-item">
-                                    <div className="manifesto-box" data-num="06">
-                                        <div className="manifesto-box-title">
-                                            <h3 className="title-3">We express</h3>
-                                        </div>
-                                        <div className="manifesto-box-description">
-                                            <p>We express specifically that our models or their outputs are not intended to be used as financial advice</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Manifesto />
         </section>
     )
 
--- src/pages/resources/Resources.js
+++ src/pages/resources/Resources.js
@@ -4,6 +4,8 @@ import './resources.scss';
 import icon1 from '../../assets/images/icons/subscribers.png';
 import icon2 from '../../assets/images/icons/lab.png';
 import icon3 from '../../assets/images/icons/research.png';
+import whoPhoto from '../../assets/images/who-photo.png';
+import Manifesto from "../about/manifesto/Manifesto";
 
 const Resources = () => {
 
@@ -11,6 +13,7 @@ const Resources = () => {
         <section className="section hero inner-page">
             <Mission />
             <div className="funding-model">
+                <div className="abstract __6" />
                 <div className="container">
                     <div className="row">
                         <div className="col-12">
@@ -36,58 +39,105 @@ const Resources = () => {
                                             <p className="regular">Zero Theorem</p>
                                         </div>
                                     </div>
-                                    <div className="model-row">
-                                        <div className="model-row-item">
-                                            <div className="model-row-box">
-                                                <p>$$$</p>
-                                                <div className="model-arrow right" />
+                                    <div className="model-body">
+                                        <div className="model-row">
+                                            <div className="model-row-item">
+                                                <div className="model-row-box">
+                                                    <p>$$$</p>
+                                                    <div className="model-arrow right" />
+                                                </div>
+                                            </div>
+                                            <div className="model-row-item">
+                                                <div className="model-row-box">
+                                                    <p>$</p>
+                                                    <div className="model-arrow right" />
+                                                </div>
+                                                <div className="model-row-box">
+                                                    <p>Infrastructure</p>
+                                                </div>
+                                                <div className="model-row-box">
+                                                    <p>Data & Models</p>
+                                                    <div className="model-arrow right" />
+                                                </div>
                                             </div>
                                         </div>
-                                        <div className="model-row-item">
-                                            <div className="model-row-box">
-                                                <p>$</p>
-                                                <div className="model-arrow right" />
+                                        <div className="model-row">
+                                            <div className="model-row-detail">
+                                                <img src={icon1} alt="Subscribers" />
+                                                <p className="regular">Subscribers</p>
                                             </div>
                                             <div className="model-row-box">
-                                                <p>Infrastructure</p>
-                                            </div>
-                                            <div className="model-row-box">
-                                                <p>Data & Models</p>
-                                                <div className="model-arrow right" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="model-row">
-                                        <div className="model-row-detail">
-                                            <img src={icon1} alt="Subscribers" />
-                                            <p className="regular">Subscribers</p>
-                                        </div>
-                                        <div className="model-row-box">
-                                            <p>Advanced Forecasts</p>
-                                            <div className="model-arrow model-arrow-left left" />
-                                        </div>
-                                        <div className="model-row-detail">
-                                            <img src={icon2} alt="Quant Lab" />
-                                            <p className="regular">Quant Lab</p>
-                                        </div>
-                                        <div className="model-row-box">
-                                            <p>Satoshi Stack Enhancements</p>
-                                            <div className="model-arrow model-arrow-left left" />
-                                        </div>
-                                        <div className="model-row-detail">
-                                            <img src={icon3} alt="Research" />
-                                            <p className="regular">Research</p>
-                                        </div>
-                                    </div>
-                                    <div className="model-row">
-                                        <div className="model-row-item">
-                                            <div className="model-row-box">
-                                                <p>Improve Baseline Valuation Model + Economic Research Pipeline</p>
+                                                <p>Advanced Forecasts</p>
                                                 <div className="model-arrow model-arrow-left left" />
+                                            </div>
+                                            <div className="model-row-detail">
+                                                <img src={icon2} alt="Quant Lab" />
+                                                <p className="regular">Quant Lab</p>
+                                            </div>
+                                            <div className="model-row-box">
+                                                <p>Satoshi Stack Enhancements</p>
+                                                <div className="model-arrow model-arrow-left left" />
+                                            </div>
+                                            <div className="model-row-detail">
+                                                <img src={icon3} alt="Research" />
+                                                <p className="regular">Research</p>
+                                            </div>
+                                        </div>
+                                        <div className="model-row">
+                                            <div className="model-row-item">
+                                                <div className="model-row-box">
+                                                    <p>Improve Baseline Valuation Model + Economic Research Pipeline</p>
+                                                    <div className="model-arrow model-arrow-left left" />
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Manifesto theme="column" />
+            <div className="who">
+                <div className="abstract __16" />
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="hero-title">
+                                <p className="subtitle">???Whowhowho???</p>
+                                <h3 className="title-1 title-border">Who are we?</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-xl-9 offset-xl-1">
+                            <div className="who-quote regular">
+                                <p className="who-quote-title">“It is irrelevant who we are... our models and their outputs speak for themselves. Perhaps a better question to ask ...is why?”</p>
+                                <p className="who-quote-author">Dr. Qohen Leth</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-xl-2 offset-xl-1 col-lg-3 col-md-4">
+                            <div className="who-preview">
+                                <div className="who-preview-icon" />
+                                <div className="who-preview-icon" />
+                                <div className="who-preview-icon" />
+                                <div className="who-preview-icon" />
+                                <img src={whoPhoto} alt="Who are we" />
+                            </div>
+                        </div>
+                        <div className="col-xl-6 offset-xl-1 col-lg-9 col-md-8">
+                            <div className="who-content">
+                                <p>We suffered... a great loss... and we struggled for a long time to understand the
+                                reason for it... the reason we were chosen, if chosen we were, to go on living with
+                                this misfortune... We came to this place... It seemed to suit us…</p>
+                                <p>Then the phone rang... We answered... A voice said, "Qohen Leth," and suddenly
+                                we felt a flash of light... We felt... a current of pure energy connecting us to
+                                something... something greater…</p>
+                                <p>We suddenly knew clearly that we only had to answer, say "Yes," and the voice
+                                would reveal the purpose behind our loss… would give us a reason to go on...</p>
                             </div>
                         </div>
                     </div>
