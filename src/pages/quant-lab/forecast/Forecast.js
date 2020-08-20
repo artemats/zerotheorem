89b5074aa72fed10ae6fb0f58ca9d1ab0ef85cbe
artemats <artem.matseruk@gmail.com>
2020-08-20 19:44:47 +0300

    set lazy import for chart

--- src/pages/quant-lab/forecast/Forecast.js
+++ src/pages/quant-lab/forecast/Forecast.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment, lazy, Suspense } from 'react';
 import { connect } from 'react-redux';
 import { loadDashboardPageSuccess } from '../../../store/quant-nav/actions';
 import thumb from '../../../assets/images/icons/forecast-2.svg';
 import LoadingIndicator from "../../../components/loadingIndicator/LoadingIndicator";
 import ErrorBoundry from "../../../components/error-boundry/ErrorBoundry";
-import TrendPlot from "../../../components/charts/trendPlot/TrendPlot";
+const TrendPlot = lazy(() => import("../../../components/charts/trendPlot/TrendPlot"));
 
 class Forecast extends Component {
 
@@ -21,64 +21,68 @@ class Forecast extends Component {
     render() {
 
         return(
-            <div className="row">
-                <div className="col-12">
-                    <div className="row">
-                        <div className="col-xl-7">
-                            <div className="dashboard-box __lg">
-                                <ErrorBoundry>
-                                    <TrendPlot />
-                                </ErrorBoundry>
+            <Fragment>
+                <div className="row">
+                    <div className="col-12">
+                        <div className="row">
+                            <div className="col-xl-7">
+                                <div className="dashboard-box __lg">
+                                    <ErrorBoundry>
+                                        <Suspense fallback={<LoadingIndicator />}>
+                                            <TrendPlot />
+                                        </Suspense>
+                                    </ErrorBoundry>
+                                </div>
                             </div>
-                        </div>
-                        <div className="col-xl-5">
-                            <div className="dashboard-box">
-                                {/*<ErrorBoundry>*/}
-                                {/*    <Suspense fallback={<LoadingIndicator />}>*/}
-                                {/*        <ResidualPlot />*/}
-                                {/*    </Suspense>*/}
-                                {/*</ErrorBoundry>*/}
+                            <div className="col-xl-5">
+                                <div className="dashboard-box">
+                                    {/*<ErrorBoundry>*/}
+                                    {/*    <Suspense fallback={<LoadingIndicator />}>*/}
+                                    {/*        <ResidualPlot />*/}
+                                    {/*    </Suspense>*/}
+                                    {/*</ErrorBoundry>*/}
+                                </div>
                             </div>
-                        </div>
-                        <div className="col-xl-7">
-                            <div className="dashboard-box __stat">
-                                {/*<ErrorBoundry>*/}
-                                {/*    <Suspense fallback={<LoadingIndicator />}>*/}
-                                {/*        <MetricBox />*/}
-                                {/*    </Suspense>*/}
-                                {/*</ErrorBoundry>*/}
+                            <div className="col-xl-7">
+                                <div className="dashboard-box __stat">
+                                    {/*<ErrorBoundry>*/}
+                                    {/*    <Suspense fallback={<LoadingIndicator />}>*/}
+                                    {/*        <MetricBox />*/}
+                                    {/*    </Suspense>*/}
+                                    {/*</ErrorBoundry>*/}
+                                </div>
                             </div>
-                        </div>
-                        <div className="col-xl-5">
-                            <div className="dashboard-box __mt">
-                                {/*<ErrorBoundry>*/}
-                                {/*    <Suspense fallback={<LoadingIndicator />}>*/}
-                                {/*        <ResidualHistogram />*/}
-                                {/*    </Suspense>*/}
-                                {/*</ErrorBoundry>*/}
+                            <div className="col-xl-5">
+                                <div className="dashboard-box __mt">
+                                    {/*<ErrorBoundry>*/}
+                                    {/*    <Suspense fallback={<LoadingIndicator />}>*/}
+                                    {/*        <ResidualHistogram />*/}
+                                    {/*    </Suspense>*/}
+                                    {/*</ErrorBoundry>*/}
+                                </div>
                             </div>
-                        </div>
-                        <div className="col-xl-6">
-                            <div className="dashboard-box __sm">
-                                {/*<ErrorBoundry>*/}
-                                {/*    <Suspense fallback={<LoadingIndicator />}>*/}
-                                {/*        <QqPlot />*/}
-                                {/*    </Suspense>*/}
-                                {/*</ErrorBoundry>*/}
+                            <div className="col-xl-6">
+                                <div className="dashboard-box __sm">
+                                    {/*<ErrorBoundry>*/}
+                                    {/*    <Suspense fallback={<LoadingIndicator />}>*/}
+                                    {/*        <QqPlot />*/}
+                                    {/*    </Suspense>*/}
+                                    {/*</ErrorBoundry>*/}
+                                </div>
                             </div>
-                        </div>
-                        <div className="col-xl-6">
-                            <div className="dashboard-box __sm">
-                                {/*<ErrorBoundry>*/}
-                                {/*    <Suspense fallback={<LoadingIndicator />}>*/}
-                                {/*        <ProbabilityPlot />*/}
-                                {/*    </Suspense>*/}
-                                {/*</ErrorBoundry>*/}
+                            <div className="col-xl-6">
+                                <div className="dashboard-box __sm">
+                                    {/*<ErrorBoundry>*/}
+                                    {/*    <Suspense fallback={<LoadingIndicator />}>*/}
+                                    {/*        <ProbabilityPlot />*/}
+                                    {/*    </Suspense>*/}
+                                    {/*</ErrorBoundry>*/}
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </Fragment>
         )
 
     }
